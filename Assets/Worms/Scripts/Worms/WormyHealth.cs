9098b5e1caa4b0bc1454697fee3943ec9bb42442
qpanzm <andres.lopez@brainz.co>
2021-05-14 13:21:49 -0500

    [Worms] :tada: Initial commit.

---
+++ Assets/Worms/Scripts/Worms/WormyHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Worms
+{
+    public class WormyHealth : MonoBehaviour
+    {
+        public int health;
+        public int maxHealth;
+        public Text txtHealth;
+
+        // Use this for initialization
+        void Start()
+        {
+            health = maxHealth;
+            txtHealth.text = health.ToString();
+        }
+
+        public void ChangeHealth(int change)
+        {
+            health += change;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+
+            txtHealth.text = health.ToString();
+        }
+    }
+}
